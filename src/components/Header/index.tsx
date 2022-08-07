740272bd48a41e8e204a068acb399862af7bcc03
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-08-07 18:06:12 -0300

    feat: add modal transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+//O header recebe uma funcao como parametro para abrir o modal
+//O header altera um estado do componente pai (app) via passagem de parametro de funcao
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>  
             </Content>  
